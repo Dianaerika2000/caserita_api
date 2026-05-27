4eb2319ebb4efed4e8baeccd179716a81696b752
Montaño Flores Diana Erika <dianaerika.montano16@gmail.com>
2026-05-27 16:25:39 -0300

    docs: add swagger examples for store dtos

--- src/store/dto/create-store-schedule.dto.ts
+++ src/store/dto/create-store-schedule.dto.ts
@@ -1,14 +1,25 @@
 import { IsEnum, Matches } from 'class-validator';
 
 import { DayOfWeek } from '../enums/day-of-week.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStoreScheduleDto {
+  @ApiProperty({
+    example: 'MONDAY',
+    enum: DayOfWeek,
+  })
   @IsEnum(DayOfWeek)
-  dayOfWeek: DayOfWeek;
+  dayOfWeek!: DayOfWeek;
 
+  @ApiProperty({
+    example: '08:00',
+  })
   @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
-  openTime: string;
+  openTime!: string;
 
+  @ApiProperty({
+    example: '18:00',
+  })
   @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
-  closeTime: string;
+  closeTime!: string;
 }
--- src/store/dto/create-store.dto.ts
+++ src/store/dto/create-store.dto.ts
@@ -19,60 +19,101 @@ import { PaymentType } from '../enums/payment-type.enum';
 import { SaleType } from '../enums/sale-type.enum';
 
 import { CreateStoreScheduleDto } from './create-store-schedule.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStoreDto {
+  @ApiProperty({
+    example: 'Tienda Rosita',
+  })
   @IsString()
   @IsNotEmpty()
-  name: string;
+  name!: string;
 
+  @ApiProperty({
+    example: 'rosita@gmail.com',
+  })
   @IsEmail()
-  email: string;
+  email!: string;
 
+  @ApiProperty({
+    example: '12345678',
+  })
   @IsString()
   @MinLength(8)
-  password: string;
+  password!: string;
 
+  @ApiProperty({
+    example: '76543210',
+  })
+  @IsString()
+  cellphone!: string;
+
+  @ApiProperty({
+    example: 'Galería Central',
+  })
   @IsString()
   @IsNotEmpty()
-  cellphone: string;
+  galleryName!: string;
 
+  @ApiProperty({
+    example: '12B',
+  })
   @IsString()
   @IsNotEmpty()
-  galleryName: string;
+  storeNumber!: string;
 
-  @IsString()
-  @IsNotEmpty()
-  storeNumber: string;
-
+  @ApiProperty({
+    example: 'https://maps.google.com/...',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   locationLink?: string;
 
+  @ApiProperty({
+    example: ['CASH', 'QR'],
+    enum: PaymentType,
+    isArray: true,
+  })
   @IsArray()
   @ArrayNotEmpty()
   @IsEnum(PaymentType, {
     each: true,
   })
-  paymentTypes: PaymentType[];
+  paymentTypes!: PaymentType[];
 
+  @ApiProperty({
+    example: ['WHOLESALE', 'RETAIL'],
+    enum: SaleType,
+    isArray: true,
+  })
   @IsArray()
   @ArrayNotEmpty()
   @IsEnum(SaleType, {
     each: true,
   })
-  saleTypes: SaleType[];
+  saleTypes!: SaleType[];
 
+  @ApiProperty({
+    example: true,
+  })
   @IsBoolean()
-  hasDelivery: boolean;
+  hasDelivery!: boolean;
 
+  @ApiProperty({
+    example: '21b361dd-6d5c-423c-9c18-ce8307882898',
+  })
   @IsUUID()
-  categoryId: string;
+  categoryId!: string;
 
+  @ApiProperty({
+    type: [CreateStoreScheduleDto],
+  })
   @IsArray()
   @ArrayMinSize(1)
   @ValidateNested({
     each: true,
   })
   @Type(() => CreateStoreScheduleDto)
-  schedules: CreateStoreScheduleDto[];
+  schedules!: CreateStoreScheduleDto[];
 }
